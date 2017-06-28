4616565e59d5a5bd3e8bfc17c6aeea82dc8df88e
nishun0419 <ninosekunlight@gmail.com>
2017-06-28 12:46:05 +0900

    add Adimin

--- laravel/Laravel/config/app.php
+++ laravel/Laravel/config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Encore\Admin\Providers\AdminServiceProvider::class,
 
         /*
          * Package Service Providers...
